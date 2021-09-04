b4a753075dc7b7aad9c9a5fe59f50c654ce30864
Ben <kellman.ben@gmail.com>
2021-09-04 10:12:21 -0700

    Reworking UsePackage.cmake to behave more like FetchContent

--- cmake/UsePackage.cmake
+++ cmake/UsePackage.cmake
@@ -1,34 +1,33 @@
 # https://gitlab.com/mariokonrad/timesheet/-/blob/master/cmake/UsePackage.cmake
 
-macro (use_package)
-  include (CMakeParseArguments)
-  cmake_parse_arguments (
-      ARG
-      "" # no values
-      "TARGET;GIT_TAG;LOCAL;REMOTE" # single values
-      "" # no multi values
-      ${ARGN}
-  )
+macro(use_package)
+    include(CMakeParseArguments)
+    cmake_parse_arguments(
+            ARG
+            "" # no values
+            "TARGET;GIT_REPOSITORY;GIT_TAG;GIT_SUBMODULES" # single values
+            "" # no multi values
+            ${ARGN}
+    )
 
-  file (TO_CMAKE_PATH "${ARG_LOCAL}" path)
-  if (IS_DIRECTORY "${path}")
-    set (target_repo "file://${path}")
-  else ()
-    if (CLONE_TO_LOCAL)
-      find_package (Git REQUIRED QUIET)
-      execute_process (COMMAND ${GIT_EXECUTABLE} clone "${ARG_REMOTE}" "${ARG_LOCAL}")
-      set (target_repo "file://${path}")
+    # Only clone to local dir if LOCAL_REPO_DIR exists
+    if (DEFINED LOCAL_REPO_DIR)
+        set(path "${LOCAL_REPO_DIR}/${ARG_TARGET}")
+        message(STATUS "${GIT_EXECUTABLE} clone ${ARG_GIT_REPOSITORY} ${path}")
+        if (NOT IS_DIRECTORY "${path}")
+            find_package(Git REQUIRED QUIET)
+            execute_process(COMMAND ${GIT_EXECUTABLE} clone "${ARG_GIT_REPOSITORY}" "${path}")
+        endif ()
+        set(target_repo "file://${path}")
     else ()
-      set (target_repo "${ARG_REMOTE}")
+        set(target_repo "${ARG_GIT_REPOSITORY}")
     endif ()
-  endif ()
 
-  include (FetchContent)
-  message(STATUS "${target_repo}")
-  FetchContent_Declare (${ARG_TARGET}
-                        GIT_REPOSITORY "${target_repo}"
-                        GIT_TAG ${ARG_GIT_TAG}
-                        GIT_SUBMODULES ""
-                        )
-  FetchContent_MakeAvailable (${ARG_TARGET})
-endmacro ()
+    include(FetchContent)
+    FetchContent_Declare(${ARG_TARGET}
+            GIT_REPOSITORY "${target_repo}"
+            GIT_TAG ${ARG_GIT_TAG}
+            GIT_SUBMODULES ${ARG_GIT_SUBMODULES}
+            )
+    FetchContent_MakeAvailable(${ARG_TARGET})
+endmacro()
