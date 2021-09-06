e9a8929550829c2d3695a10365e700f0ea32ac6e
Ben <kellman.ben@gmail.com>
2021-09-05 19:15:45 -0700

    Updated shell to use new fixed_string and cmd_history classes

--- include/hh/shell.hpp
+++ include/hh/shell.hpp
@@ -1,109 +1,174 @@
 /// \file shell.hpp
 /// \brief Created on 2021-08-30 by Ben
-/// \todo deal with sharing streams between classes
 
 #pragma once
-#include <hh/mini_stream.hpp>
-#include <hh/ansi_parser.hpp>
-#include <hh/ansi_codes.hpp>
-#include <hh/cmd_history.hpp>
 #include <cctype>
+#include <hh/ansi_codes.hpp>
+#include <hh/ansi_parser.hpp>
+#include <hh/cmd_history.hpp>
+#include <hh/fixed_string.h>
+#include <hh/mini_stream.hpp>
 #include <string>
 
-namespace hh::cli {
+namespace hh::shell {
 
-template<serial_io_device IO, std::size_t n>
-class shell {
-public:
-    using olstream = oserial_stream<IO>;
-    struct command {
-      using cmd_function = int(void*);
-      cmd_function* execute;
-      const char* cmd_name;
-      const char* help_message;
-    };
+    template<serial_io_device IO, std::size_t NumLines, std::size_t LineLen>
+    class shell {
+    public:
+        using olstream = oserial_stream<IO>;
+        struct command {
+            using cmd_function = int(void *);
+            cmd_function *execute;
+            const char *cmd_name;
+            const char *help_message;
+        };
 
-    olstream lout;
+        olstream lout;
 
-    explicit shell(IO& io)
-            :io_{io}, lout{io} { }
+        explicit shell(IO &io)
+            : io_{io}, lout{io} {}
 
-    shell(IO& io, command* command_table, std::size_t num_cmds)
-            :shell(io) { }
+        shell(IO &io, command *command_table, std::size_t num_cmds)
+            : shell(io) {}
 
-    [[nodiscard]] const char* current_line() const{
-        return storage_;
-    }
-
-protected:
-    // todo decide if these should just be public
-    void notify_rx(){
-        auto ch = io_.get();
-
-        while (ch != eof_){
-            process_rx_char(ch);
-            ch = io_.get();
+        [[nodiscard]] std::string_view current_line() const {
+            return currentLine_.c_str();
         }
-    }
 
-    void notify_connected() { lout << endl << welcome_message_ << endl << prompt_char_; }
-private:
+    protected:
+        // todo decide if these should just be public
+        void notify_rx() {
+            auto ch = io_.get();
 
-    enum class parser_state {
-      text,
-      ansi_cmd,
-    };
-
-    void process_rx_char(char ch)
-    {
-        switch (state_) {
-        case parser_state::text:
-            parse_text_char(ch);
-            break;
-        case parser_state::ansi_cmd:
-            if (parser_.parse(ch)) {
-                state_ = parser_state::text;
+            while (ch != eof_) {
+                process_rx_char(ch);
+                ch = io_.get();
             }
-            break;
         }
-    }
 
-    void parse_text_char(char ch)
-    {
-        switch (ch) {
-        case '\x1b':
-            state_ = parser_state::ansi_cmd;
-            parser_.reset();
-            break;
-        case '\n':
-        case '\r':
-            lout << endl << prompt_char_;
-            storage_end_ = storage_;
-            storage_[0] = '\0';
-            break;
-        case '\b':
-            if (storage_end_>storage_) {
-                *(--storage_end_) = '\0';
-                lout << ansi::save_cursor << ansi::clear_line << prompt_char_ << storage_;
-                lout << ansi::restore_cursor << ansi::move_left<1>;
+        void notify_connected() { lout << endl
+                                       << welcome_message_ << endl
+                                       << prompt_char_; }
+
+    private:
+        using history = cmd_history<NumLines, LineLen>;
+        using int_type = std::char_traits<char>::int_type;
+        using cmd_iter_type = typename history::const_iterator;
+        using string = container::fixed_string<LineLen>;
+
+        enum class parser_state {
+            text,
+            ansi_cmd,
+        };
+
+        void process_rx_char(char ch) {
+            switch (state_) {
+                case parser_state::text:
+                    parse_text_char(ch);
+                    break;
+                case parser_state::ansi_cmd:
+                    if (parser_.parse(ch)) {
+                        state_ = parser_state::text;
+                        // handle ansi command
+                        handle_ansi_cmd(parser_.code());
+                    }
+                    break;
             }
-            break;
-        default:
-            lout << ch;
-            *storage_end_++ = ch;
-            break;
         }
-    }
 
-    IO& io_;
-    using int_type = std::char_traits<char>::int_type;
-    static constexpr int_type eof_ = std::char_traits<char>::eof();
-    const char prompt_char_{'>'};
-    const char* welcome_message_{""};
-    const char* endl{"\n\r"};
-    char storage_[n]{};
-    char* storage_end_{storage_};
-    parser_state state_{parser_state::text};
-    ansi::parser parser_{};
-};
+        void handle_ansi_cmd(const ansi::parser::ansi_code &code) {
+            switch (code.control_char) {
+                case 'A':
+                    // up
+                    if (!onFirstCmd_) {
+                        currentLine_.clear();
+                        currentLine_.append(prevCommand_->data());
+                        lout << ansi::clear_line << prompt_char_;
+                        lout << prevCommand_->data();
+                        if (prevCommand_ == history_.begin()) {
+                            onFirstCmd_ = true;
+                        } else {
+                            onFirstCmd_ = false;
+                            --prevCommand_;
+                        }
+                    }
+
+                    break;
+                case 'B':
+                    // down
+                    {
+                        auto it = prevCommand_;
+                        ++it;
+                        if (!onFirstCmd_ && it != history_.end()) {
+                            ++it;
+                        }
+                        if (it != history_.end()) {
+                            currentLine_.clear();
+                            currentLine_.append(it->data());
+                            lout << ansi::clear_line << prompt_char_;
+                            lout << it->data();
+                            prevCommand_ = it;
+                        }
+                    }
+                    onFirstCmd_ = false;
+                    break;
+                case 'C':
+                    // right
+                    cursor_ += code.params[0];
+                    lout << "\x1b[" << code.params[0] << 'C';
+                    break;
+                case 'D':
+                    cursor_ -= code.params[0];
+                    lout << "\x1b[" << code.params[0] << 'D';
+                    // left
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void parse_text_char(char ch) {
+            switch (ch) {
+                case '\x1b':
+                    state_ = parser_state::ansi_cmd;
+                    parser_.reset();
+                    break;
+                case '\n':
+                case '\r':
+                    lout << endl
+                         << prompt_char_;
+                    history_.push_back(currentLine_.c_str());
+                    prevCommand_ = history_.end();
+                    --prevCommand_;
+                    currentLine_.clear();
+                    cursor_ = currentLine_.begin();
+                    break;
+                case '\b':
+                    if (!currentLine_.empty()) {
+                        currentLine_.erase(--cursor_);
+                        lout << ansi::save_cursor << ansi::clear_line << prompt_char_ << currentLine_.c_str();
+                        lout << ansi::restore_cursor << ansi::move_left<1>;
+                    }
+                    break;
+                default:
+                    lout << ch;
+                    currentLine_.insert(cursor_, ch);
+                    ++cursor_;
+                    break;
+            }
+        }
+
+        IO &io_;
+        static constexpr int_type eof_ = std::char_traits<char>::eof();
+        const char prompt_char_{'>'};
+        const char *welcome_message_{""};
+        const char *endl{"\n\r"};
+        history history_;
+        string currentLine_;
+        cmd_iter_type prevCommand_{history_.begin()};
+        bool onFirstCmd_ = false;
+        const char *cursor_{currentLine_.begin()};
+        parser_state state_{parser_state::text};
+        ansi::parser parser_{};
+    };
 }
--- test/test_shell.cpp
+++ test/test_shell.cpp
@@ -6,6 +6,8 @@
 #include <hh/shell.hpp>
 #include <sstream>
 
+using namespace std::string_literals;
+
 struct mock_serial {
   std::stringstream istream{};
   std::stringstream ostream{};
@@ -23,7 +25,7 @@ struct mock_serial {
   }
 };
 
-using shell_t = hh::cli::shell<mock_serial, 128>;
+using shell_t = hh::shell::shell<mock_serial, 10, 64>;
 
 struct shell_test_t : public shell_t {
   using shell_t::shell_t;
@@ -31,7 +33,7 @@ struct shell_test_t : public shell_t {
   void connect() { notify_connected(); }
 };
 
-TEST_CASE("shell echo alpha numeric", "[shell][echo]")
+TEST_CASE("inputted alpha numeric chars are echoed to serial output", "[shell][echo]")
 {
     mock_serial serial;
     shell_test_t shell{serial};
@@ -44,7 +46,7 @@ TEST_CASE("shell echo alpha numeric", "[shell][echo]")
     CHECK(shell.current_line()==str);
 }
 
-TEST_CASE("shell newline", "[shell]")
+TEST_CASE("newline char on non empty line outputs NL-CR and clears current line", "[shell]")
 {
     mock_serial serial;
     shell_test_t shell{serial};
@@ -57,7 +59,7 @@ TEST_CASE("shell newline", "[shell]")
     CHECK(shell.current_line()==std::string{""});
 }
 
-TEST_CASE("shell backspace", "[shell]")
+TEST_CASE("backspace on non empty line deletes last char", "[shell]")
 {
     mock_serial serial;
     shell_test_t shell{serial};
@@ -74,7 +76,7 @@ TEST_CASE("shell backspace", "[shell]")
     CHECK(shell.current_line()==std::string{"hello world"});
 }
 
-TEST_CASE("shell backspace overflow", "[shell]")
+TEST_CASE("backspace on empty line has no effect", "[shell]")
 {
     mock_serial serial;
     shell_test_t shell{serial};
@@ -91,7 +93,7 @@ TEST_CASE("shell backspace overflow", "[shell]")
     CHECK(shell.current_line()==std::string{"hi!"});
 }
 
-TEST_CASE("shell move cursor and backspace", "[shell]")
+TEST_CASE("left ansi code followed by backspace deletes second to last char", "[shell]")
 {
     mock_serial serial;
     shell_test_t shell{serial};
@@ -104,4 +106,102 @@ TEST_CASE("shell move cursor and backspace", "[shell]")
                                 "\x1b[s\x1b[2K>hello world 4!\x1b[u\x1b[1D");
     CHECK(shell.current_line()==std::string{"hello world 4!"});
 
+}
+
+SCENARIO("up and down ansi codes allow next a previous commands to be accessed and modified", "[shell][history"){
+    GIVEN("A shell with a non empty command history"){
+        mock_serial serial;
+        shell_test_t shell{serial};
+
+        auto up_code = "\x1b[1A"s;
+        auto down_code = "\x1b[1B"s;
+
+        std::string cmd1{"cmd1"};
+        std::string cmd2{"cmd2"};
+        std::string cmd3{"cmd3"};
+        std::string cmd4{"cmd4"};
+
+        serial.istream << cmd1 << '\n' << cmd2 << '\n' << cmd3 << '\n' << cmd4 << '\n';
+        shell.notify();
+        serial.ostream = std::stringstream{};
+        serial.istream.clear();
+
+        WHEN("a up ansi code is received"){
+            serial.istream << up_code;
+            shell.notify();
+
+            THEN("the previous command is displayed"){
+                CHECK(serial.ostream.str() == "\x1B[2K>cmd4");
+                CHECK(shell.current_line() == cmd4);
+            }
+
+            AND_WHEN("the command is modified"){
+                THEN("the modified command is appended to the history"){
+
+                }
+            }
+        }
+
+        WHEN("two up ansi codes are received"){
+            serial.istream << up_code << up_code;
+            shell.notify();
+
+            THEN("the second to most recent command is displayed"){
+                CHECK(serial.ostream.str() == "\x1B[2K>cmd4\x1B[2K>cmd3");
+                CHECK(shell.current_line() == cmd3);
+            }
+
+            WHEN("a down ansi code is received"){
+                serial.istream.clear();
+                serial.istream << down_code;
+                shell.notify();
+                THEN("the previous command is displayed"){
+                    CHECK(serial.ostream.str() == "\x1B[2K>cmd4\x1B[2K>cmd3\x1B[2K>cmd4");
+                    CHECK(shell.current_line() == cmd4);
+                }
+            }
+        }
+
+        WHEN("many up ansi codes are received"){
+            serial.istream << up_code << up_code << up_code << up_code << up_code;
+            shell.notify();
+
+            THEN("the oldest stored command is displayed"){
+                CHECK(serial.ostream.str() == "\x1B[2K>cmd4\x1B[2K>cmd3\x1B[2K>cmd2\x1B[2K>cmd1");
+                CHECK(shell.current_line() == cmd1);
+            }
+
+            AND_WHEN("a down ansi code is received"){
+                serial.istream.clear();
+                serial.istream << down_code;
+                shell.notify();
+
+                THEN("the second oldest stored command is displayed"){
+                    CHECK(serial.ostream.str() == "\x1B[2K>cmd4\x1B[2K>cmd3\x1B[2K>cmd2\x1B[2K>cmd1\x1B[2K>cmd2");
+                    CHECK(shell.current_line() == cmd2);
+                }
+            }
+        }
+
+        WHEN("a down ansi code is received"){
+            serial.istream << down_code;
+            shell.notify();
+            THEN("nothing changes"){
+                CHECK(serial.ostream.str() == "");
+                CHECK(shell.current_line().empty());
+            }
+            AND_WHEN("a up ansi code is received"){
+                serial.istream.clear();
+                serial.istream << up_code;
+                shell.notify();
+                THEN("the previous command is displayed"){
+                    CHECK(serial.ostream.str() == "\x1B[2K>cmd4");
+                    CHECK(shell.current_line() == cmd4);
+                }
+            }
+        }
+
+        // todo modification and command entered
+        // todo repeated command
+    }
 }
